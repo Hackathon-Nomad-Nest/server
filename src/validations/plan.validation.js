849bc3e7c2277d4f8e141d111c8dc71489cc7b58
Pragati Kesarwani <pragati.kesarwani@unthinkable.co>
2024-09-15 07:46:18 +0530

    Updated the tips and music payload to be accepted

--- src/validations/plan.validation.js
+++ src/validations/plan.validation.js
@@ -56,21 +56,16 @@ const getPlans = {
 };
 
 const getTipsAndMusic = {
-  body: Joi.object().keys({
-    travelInput: Joi.object()
-      .keys({
-        to: Joi.string(),
-        from: Joi.string().required(), // Departure field
-        budget: Joi.string().required(), // Budget range
-        startDate: Joi.string().required(), // Trip start date
-        adults: Joi.number().integer().min(1).required(), // Number of adults
-        kids: Joi.number(), // Number of adults
-        numberOfDays: Joi.number().integer().min(1).required(), //  for days
-        tripType: Joi.string().required(), // Trip type
-        preferredTravelMode: Joi.string(), // Preferred travel mode
-      })
-      .required(),
-  }),
+  body: Joi.object()
+    .keys({
+      to: Joi.string(),
+      from: Joi.string().required(), // Departure field
+      startDate: Joi.string().required(), // Trip start date
+      endDate: Joi.string().required(), // Trip end date
+      tripType: Joi.string().required(), // Trip type
+      preferredTravelMode: Joi.string(), // Preferred travel mode
+    })
+    .required(),
 };
 
 module.exports = {
@@ -79,5 +74,5 @@ module.exports = {
   addOrRemoveAnActivity,
   updateMembers,
   getPlans,
-  getTipsAndMusic
+  getTipsAndMusic,
 };
